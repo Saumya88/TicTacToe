7bbc892ebc5871817c8ab07696624d6971744de1
Saumya88 <singsaumya9@gmail.com>
2021-05-22 22:16:13 +0530

    Made diiferent methods in Board class to get the position from user and mark it on the board. DRAFT: Code to switch the players from user to computer and computer to user

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,22 +1,117 @@
 package board;
-public class Board {
-	private char[][] board;
-	public Board() {
+import java.util.*;
+public class Board 
+{
+	private char[][] board; //array to store the positions of TicTacToe Board
+	public Board() //class constructor to initialize the board
+	{
 		board = new char[3][3];
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
+		for(int i=0;i<3;i++) 
+		{
+			for(int j=0;j<3;j++) 
+			{
 				board[i][j] = '-';
 			}
 		}
 	}
-	public void showBoard() {
-		System.out.println("             1  2  3");
-		for(int i=0;i<3;i++) {
-			System.out.print("          " + (i+1) + "  ");
-			for(int j=0;j<3;j++) {
+	public void showBoard() //method to print the board on screen
+	{
+		System.out.println("             1  2  3"); // printing the column number
+		for(int i=0;i<3;i++) 
+		{
+			System.out.print("          " + (i+1) + "  ");// printing the row number
+			for(int j=0;j<3;j++) 
+			{
 				System.out.print(board[i][j] + "  ");
 			}
 			System.out.println();
 		}
 	}
+	public int getPosition(String playerType) // method to get the position from user or computer 
+	{
+		if(playerType=="User") // if the player type is user the position must be entered by the user
+		{
+			int position;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the position you want to mark");
+			position=sc.nextInt();
+			System.out.println(position);
+			return position;
+		}
+		else //if the player type is computer then random numbers must be generated as position
+		{
+			int position;
+			Random random = new Random();   
+			// Generates random integers 0 to 8 
+			position = random.nextInt(9);  
+			return position + 1;
+			
+		}
+				
+	}
+	
+	public void setPosition() // method to the set the position on the board
+	{
+		int position;
+		String player="User";
+		char symbol='X';
+		
+		System.out.println("\n \n ***************Game starts************ \n \n");
+		showBoard();
+		System.out.println("\nUser will go first \n");
+
+		while(true)
+		{
+			// assigning the symbol on the basis of player type
+			if(player=="User")
+			{
+				symbol='X';
+				
+			}
+			if(player=="Computer")
+			{
+				symbol='O';
+			}
+			position=getPosition(player);// calling the method to get the position to be marked
+			switch(position)
+			{
+				case 1: board[0][0]=symbol;
+				break;
+				case 2: board[0][1]=symbol;
+				break;
+				case 3: board[0][2]=symbol;
+				break;
+				case 4: board[1][0]=symbol;
+				break;
+				case 5: board[1][1]=symbol;
+				break;
+				case 6: board[1][2]=symbol;
+				break;
+				case 7: board[2][0]=symbol;
+				break;
+				case 8: board[2][1]=symbol;
+				break;
+				case 9: board[2][2]=symbol;
+				break;
+				default:
+				break;
+			
+			}
+			showBoard();
+			
+			// trying to change the player type from user to computer and vice versa
+			// not working 
+			// possible reason: Strings are immutable in java
+			if(player.compareTo("User")==0)
+			{
+				player="Computer";
+			}
+			if(player.compareTo("Computer")==0)
+			{
+				player="User";
+			}
+		
+		}
+	}
 }
+
--- src/board/Main.java
+++ src/board/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Board board = new Board();
-		board.showBoard();
+		board.setPosition();
 	}
 
 }
